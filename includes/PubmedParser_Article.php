698d430b6b23b2b19cb771a62654518b242aebda
Vedmaka <god.vedmaka@gmail.com>
2020-03-22 17:49:07 +0300

    Article allauthors() to return unmodified values

--- includes/PubmedParser_Article.php
+++ includes/PubmedParser_Article.php
@@ -1,26 +1,26 @@
 <?php
 /*
  *      \file PubmedParser.Article.php
- *      
+ *
  *      Copyright 2011-2019 Daniel Kraus <krada@gmx.net>
- *      
+ *
  *      This program is free software; you can redistribute it and/or modify
  *      it under the terms of the GNU General Public License as published by
  *      the Free Software Foundation; either version 2 of the License, or
  *      (at your option) any later version.
- *      
+ *
  *      This program is distributed in the hope that it will be useful,
  *      but WITHOUT ANY WARRANTY; without even the implied warranty of
  *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *      GNU General Public License for more details.
- *      
+ *
  *      You should have received a copy of the GNU General Public License
  *      along with this program; if not, write to the Free Software
  *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  *      MA 02110-1301, USA.
  */
 namespace PubmedParser;
- 
+
 if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 'This is an extension to MediaWiki and cannot be run standalone.' );
 }
@@ -110,11 +110,11 @@ class Article
 		}
 	}
 
-	/** Parse the authors node and build an array of last names and initials. 
+	/** Parse the authors node and build an array of last names and initials.
 	 * If a collective name is encountered, save it.
 	 */
 	protected function parseAuthors( \XMLReader $reader ) {
-		// Loop over the children of the AuthorList node and stop at the closing 
+		// Loop over the children of the AuthorList node and stop at the closing
 		// tag of the AuthorList node.
 		while ( $reader->read() && ! ( $reader->name === 'AuthorList' ) ) {
 			if ( $reader->nodeType == \XMLReader::ELEMENT ) {
@@ -132,9 +132,9 @@ class Article
 		}
 	}
 
-	/** Parses the publication date (PubmedArticleSet -> PubmedArticle -> 
+	/** Parses the publication date (PubmedArticleSet -> PubmedArticle ->
 	 * MedlineCitation -> Article -> Journal -> JournalIssue -> PubDate).
-	 * Sometimes, there will be no such node; in these cases, we use the 
+	 * Sometimes, there will be no such node; in these cases, we use the
 	 * 'MedlineDate' node that is a child of 'JournalIssue'.
 	 */
 	protected function parseDate( \XMLReader $reader) {
@@ -144,14 +144,14 @@ class Article
 					$this->year = $reader->readInnerXML();
 				}
 				elseif ( $reader->name === 'MedlineDate' && !$this->year ) {
-					// If we read the year of publication from the MedlineDate node, 
+					// If we read the year of publication from the MedlineDate node,
 					// we need to extract a four-digit string from this node's value.
 					if ( preg_match( '/\d{4}/', $reader->readInnerXML(), $matches ) ) {
 						$this->year = $matches[0];
 					}
 				}
 			}
-		}	
+		}
 	}
 
 	/** Returns an abbreviated list of the authors. If there are two
@@ -172,7 +172,7 @@ class Article
 		} else {
 			return $this->collectiveName;
 		}
-	} 
+	}
 
 	/** Returns a list of all authors of this article.
 	 *  @param $useInitials [in] Boolean; if True, initials will be appended
@@ -181,13 +181,13 @@ class Article
 		$numAuthors = count( $this->authors );
 		$a = '';
 		if ( $numAuthors > 1 ) {
-			for ( $i=0; $i < $numAuthors-1; $i++ ) {
+			for ( $i=0; $i < $numAuthors; $i++ ) {
 				$a .= $this->authorName( $i, $useInitials ) . ', ';
 			}
-			// Cut off the last ", ", add the "and" character or word, and append 
+			// Cut off the last ", ", add the "and" character or word, and append
 			// the last author's name.
-			$a = substr( $a, 0, strlen( $a )-2 ) . ' ' . Extension::$and 
-				. ' ' . $this->authorName( $i, $useInitials );
+			//$a = substr( $a, 0, strlen( $a )-2 ) . ' ' . Extension::$and
+			//	. ' ' . $this->authorName( $i, $useInitials );
 		} elseif ( $numAuthors == 1 ) {
 			$a = $this->authorName( 0, $useInitials );
 		} else {
@@ -220,7 +220,7 @@ class Article
 	/** A private function that returns either the author's last name or
 	 * the "CollectiveName" is the author is a group.
 	 * \param $index Index of the author
-	 * \returns either the author's last name, or the group's collective name 
+	 * \returns either the author's last name, or the group's collective name
 	 */
 	private function authorName( $index, $useInitial = false ) {
 		if ( $index < count( $this->authors ) ) {
@@ -230,10 +230,10 @@ class Article
 				$iarray = str_split($i, 1);
 				$i = implode( Extension::$initialPeriod, $iarray)
 					. Extension::$initialPeriod;
-				// Spaces in the "Pubmedparser-initialperiod" system message must be 
-				// encoded as "&nbsp;", lest they be removed by MediaWiki's text 
-				// processing. In order to remove the trailing "&nbsp;" after 
-				// concatenating all authors and initials, we use the trim function 
+				// Spaces in the "Pubmedparser-initialperiod" system message must be
+				// encoded as "&nbsp;", lest they be removed by MediaWiki's text
+				// processing. In order to remove the trailing "&nbsp;" after
+				// concatenating all authors and initials, we use the trim function
 				// with " \xc2\xa0".
 				$author = trim( $author . Extension::$initialSeparator
 					. ' ' . $i, " \xc2\xa0");
